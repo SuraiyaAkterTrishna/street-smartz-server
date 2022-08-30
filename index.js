549a59bafcf3549ef1de3247b455357cdae23483
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-08-31 01:29:53 +0600

    update inventory complete

--- index.js
+++ index.js
@@ -40,6 +40,28 @@ async function run(){
           res.send(result);
       });
 
+      // update inventory
+      app.put('/inventory/:id', async(req, res) =>{
+        const id = req.params.id;
+        const updateItem = req.body;
+        const filter = {_id: ObjectId(id)};
+        const options = { upsert: true};
+        const updateDoc = {
+            $set: {
+              name: updateItem.name,
+              img: updateItem.img,
+              details: updateItem.details,
+              price: updateItem.price,
+              quantity: updateItem.quantity,
+              supplier: updateItem.supplier,
+              brand: updateItem.brand,
+              mileage: updateItem.mileage
+            }
+        };
+        const result = await itemsCollection.updateOne(filter, updateDoc, options);
+        res.send(result);
+    });
+
     }
     finally{
 
