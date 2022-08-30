5b79fc44693df21161f5c5f37487b8eca608a1a1
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-08-31 01:25:49 +0600

    add items collection

--- index.js
+++ index.js
@@ -17,67 +17,7 @@ async function run(){
         await client.connect();
         const itemsCollection = client.db('street-smartz').collection('items');
 
-        // create inventory
-        app.post('/inventory', async (req, res)=>{
-          const addItem = req.body;
-          const result = await itemsCollection.insertOne(addItem);
-          res.send(result);
-      });
-
-        // get inventory
-        app.get('/inventory',async(req,res)=>{
-            const query = {};
-            const cursor = itemsCollection.find(query);
-            const items = await cursor.toArray();
-            res.send(items);
-        });
-
-        // get single inventory
-        app.get('/inventory/:id', async(req, res) =>{
-          const id = req.params.id;
-          const query = {_id: ObjectId(id)};
-          const result = await itemsCollection.findOne(query);
-          res.send(result);
-      });
-
-      // update inventory
-      app.put('/inventory/:id', async(req, res) =>{
-        const id = req.params.id;
-        const updateItem = req.body;
-        const filter = {_id: ObjectId(id)};
-        const options = { upsert: true};
-        const updateDoc = {
-            $set: {
-              name: updateItem.name,
-              img: updateItem.img,
-              details: updateItem.details,
-              price: updateItem.price,
-              quantity: updateItem.quantity,
-              supplier: updateItem.supplier,
-              brand: updateItem.brand,
-              mileage: updateItem.mileage
-            }
-        };
-        const result = await itemsCollection.updateOne(filter, updateDoc, options);
-        res.send(result);
-    });
-
-    // delete inventory
-    app.delete('/inventory/:id', async(req, res) =>{
-      const id = req.params.id;
-      const query = {_id: ObjectId(id)};
-      const result = await itemsCollection.deleteOne(query);
-      res.send(result);
-    });
-
-    // inventory collection api
-    app.get('/my-inventory/', async(req, res)=>{
-      const email = req.query.email;
-      const query = {email: email};
-      const cursor = itemsCollection.find(query);
-      const items = await cursor.toArray();
-      res.send(items);
-    });
+        
 
     }
     finally{
