bdacff26b8186e0b112667bb70195c37edf96711
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2022-08-31 01:30:52 +0600

    delete inventory complete

--- index.js
+++ index.js
@@ -62,6 +62,14 @@ async function run(){
         res.send(result);
     });
 
+    // delete inventory
+    app.delete('/inventory/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await itemsCollection.deleteOne(query);
+      res.send(result);
+    });
+
     }
     finally{
 
